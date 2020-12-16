18faccfa5df82a6076571b11e4bb4fef1e9db203
samuel <damosisamuel@gmail.com>
2020-12-16 16:31:59 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -28,13 +28,45 @@ console.log(message + " and your score is " + player );
   document.getElementById("computer").innerHTML = "computer score is " + computer + " " + message;
 
   console.log(message + " and your score is " + computer);
-} else if(computer == player) {
-  message = "draw try again";
-  document.getElementById("draw").innerHTML = message;
-  console.log(message);
+} else if (computer == player) {
+  message = "draw";
+  // document.getElementById("drawi").innerHTML = message + "try again";
+  console.log("play again " + message);
 }
 
-// document.getElementById("genera").innerHTML = player + computer;
+
+}
+);
+
+document.getElementById("cancel").addEventListener('click',
+function() {
+
+
+
+  var player ='';
+  var computer ="";
+  // first ID player & computer random number by applying this belwo
+
+  // so the random number generated has kindly been saved in the variables player + computer. so by applying this methid we want it to show on our id html page the results
+  document.getElementById("player").innerHTML = "";
+  document.getElementById("computer").innerHTML ='';
+  // so the random number generated has kindly been saved in the variables player + computer. so by applying this methid we want it to show on our id html page the results
+
+
+
+  // we apply the the if condition to decide when its equal to or greater than or else by telling it what to do from the applied version done previously above
+  if (player > computer) {
+  document.getElementById("player").innerHTML ='';
+  } else if (computer > player) {
+    document.getElementById("computer").innerHTML ="";
+
+  }
+
+
+  document.getElementById("cancel").className = cancel;
+
+
+
 
 }
 );
